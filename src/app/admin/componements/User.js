a560adde9f78d26d9941749770bf6eab4eb61533
kcmki <mekkimohe@gmail.com>
2024-02-06 20:15:05 +0100

    preparing user page and stats

--- src/app/admin/componements/User.js
+++ src/app/admin/componements/User.js
@@ -31,6 +31,121 @@ export default function User({setState}){
     return (
         <div className="flex flex-col justify-center items-center w-full">
             <GoBack setState={setState}/>
+            <UserPower />
+
+            {
+                localStorage.getItem('userRole') === 'admin' ? <AdminPower /> : null
+            }
         </div>
+    )
+}
+
+function UserPower(){
+
+    return(
+        <>
+            <UserStats />
+            <div className="p-2 m-2 w-full bg-white text-black rounded-xl">
+                <h1 className="text-2xl text-center">Update (with password)</h1>
+            </div>
+        </>
+    )
+}
+
+function UserStats(){
+    const setLogged = useContext(LoggedContext);
+    const [data, setData] = useState(undefined);
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        let fetchData = async () => {
+            let res = await fetch('http://localhost:3000/api/user', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + localStorage.getItem('sessionId')
+                }});
+            let response = await res.json();
+
+            if (response.success) 
+            {
+                setData(response.data);
+
+            }else{
+                if (response.message == "Invalid session") {setLogged(false);localStorage.setItem('sessionId','');}
+                alert(response.message);
+            }
+
+            setLoading(false);
+            console.log(data);
+
+        }
+        fetchData();
+    },[])
+
+    if (loading) return (<div className='flex justify-center align-center bg-white rounded-xl p-2 m-2 w-full'> 
+                            <Oval
+                            visible={true}
+                            height="100"
+                            width="100"
+                            color="#000"
+                            secondaryColor="#000"
+                            ariaLabel="oval-loading"
+                            wrapperStyle={{}}
+                            wrapperClass=""
+                            />
+                        </div>)
+    if (!loading && data ==undefined) return (<div className="p-2 m-2 w-full bg-white text-black rounded-xl text-center">No data</div>)
+    return(
+    <div className="p-2 m-2 w-full bg-white text-black rounded-xl flex flex-col justify-start align-center">
+        <div className="grid grid-cols-2" >
+            <div className="mx-10"> 
+                <div>user name : <span className="username">{data["nom"]}</span></div>
+                <div>user mail : <span className="username">{data["email"]}</span></div>
+                <div>user role : <span className="username">{data["role"]}</span></div>
+            </div>
+            <table className="table-auto border-spacing-0 border-2 border-black mx-10">
+                <thead>
+                    <tr>
+                        <th className="border-2 border-black">Stat</th>
+                        <th className="border-2 border-black ">Moi</th>
+                        <th className="border-2 border-black ">Total</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td className="border-2 border-black">CA/mois</td>
+                        <td className="border border-black">{data["CA/M"][0]}</td>
+                        <td className="border border-black">{data["CA/M"][1]}</td>
+                    </tr>
+                    <tr>
+                        <td className="border-2 border-black">Commande/semaine</td>
+                        <td className="border border-black">{data["CM/S"][0]}</td>
+                        <td className="border border-black">{data["CM/S"][1]}</td>
+                    </tr>
+                    <tr>
+                        <td className="border-2 border-black">Commande/mois</td>
+                        <td className="border border-black">{data["CM/M"][0]}</td>
+                        <td className="border border-black">{data["CM/M"][1]}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    )
+}
+
+function AdminPower(){
+    return(
+        <>
+            <div className="p-2 m-2 w-full bg-white text-black rounded-xl">
+                <h1 className="text-2xl text-center">Update site (with password)</h1>
+            </div>
+            <div className="p-2 m-2 w-full bg-white text-black rounded-xl">
+                <h1 className="text-2xl text-center">Add user</h1>
+            </div>
+            <div className="p-2 m-2 w-full bg-white text-black rounded-xl">
+                <h1 className="text-2xl text-center">Delete user (with password)</h1>
+            </div>
+        </>
     )
 }
--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -2,13 +2,79 @@
 
 import { NextResponse } from 'next/server'
 import db from '/lib/db'
+import { group } from 'console';
 
 // endpoint to create a user using email, password and userRole (has to be admin and logged in)
 
 export async function GET(req) {
-    return NextResponse.json(
-      { success: false , message: 'use POST method to login'}
-    )
+    let reqSessionid = req.headers.get('Authorization').split(" ")[1];
+    let dataSession = await db.collection('sessions').findOne({sessionId:reqSessionid});
+    //check session and create new one
+    if(!dataSession || dataSession.expirationTime < Date.now()){
+        try{
+            await db.collection('sessions').deleteOne({sessionId:reqSessionid})
+        }catch(e){}
+        return NextResponse.json(
+            {success:false,message:"Invalid session"}
+        )
+    }
+
+    
+    // retrieving data
+    try{
+        let user = await db.collection('users').findOne({userId:dataSession.userId});
+        let data = {nom:user.name,email:user.email,role:user.userRole}
+        data["CM/S"] = [0,0]
+        data["CM/M"] = [0,0]
+        data["CA/M"] = [0,0]
+        
+        data["CM/S"][1] = await db.collection('orders').find({date:{$gte:Date.now()-604800000}}).count();
+        data["CM/S"][0] = await db.collection('orders').find({userId:user.userId,date:{$gte:Date.now()-604800000}}).count();
+        
+
+        data["CM/M"][1] = await db.collection('orders').find({date:{$gte:Date.now()-2592000000}}).count();
+        data["CM/M"][0] = await db.collection('orders').find({userId:user.userId,date:{$gte:Date.now()-2592000000}}).count();
+    
+        let groupResult = await db.collection('orders').aggregate([
+                        {
+                            $match: {
+                                date: { $gte: Date.now() - 2592000000 }
+                            }
+                        },
+                        {
+                            $group: {
+                                _id: null,
+                                totalSum: { $sum: "$totalPrice" }
+                            }
+                        }
+                    ]).toArray();
+        groupResult = groupResult[0]
+        data["CA/M"][1] = groupResult != undefined ? groupResult["totalSum"] : 0;
+        groupResult = await db.collection('orders').aggregate([
+                        {
+                            $match: {
+                                date: { $gte: Date.now() - 2592000000 },
+                                userId: user.userId
+                            }
+                        },
+                        {
+                            $group: {
+                                _id: null,
+                                totalSum: { $sum: "$totalPrice" }
+                            }
+                        }
+                    ]).toArray();
+        groupResult = groupResult[0]
+        data["CA/M"][0] = groupResult != undefined ? groupResult["totalSum"] : 0;
+        
+        return NextResponse.json(
+          { success: true , message: 'Data is succesfully sent',data:data}
+        )
+    }catch(e){
+        return NextResponse.json(
+            {success:false,message:"Couldn't retrieve data : "+e.message}
+        )
+    }
 }
 
 export async function POST(req) {
@@ -44,7 +110,7 @@ export async function POST(req) {
     let password = requestjson.password;
     let userRole = requestjson.userRole;
 
-    let userId = await db.collection('users').find({}).count()
+    let userId = hash(email);
     
     try{
         db.collection('users').insertOne({userId:userId,email:email,password:password,userRole:userRole}, function(err, res) {
@@ -60,4 +126,16 @@ export async function POST(req) {
     return NextResponse.json(
         {success:true,message:"User added"}
     )
+}
+
+
+
+const { createHash } = require('crypto');
+
+function hash(string) {
+  return createHash('sha256').update(string).digest('hex');
+}
+
+function generateSessionId(userId, timestamp) {
+  return hash(userId + timestamp)
 }
